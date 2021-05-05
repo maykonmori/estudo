1d41704b3c026b6a0ce2790a5d3d67ce0a994d8c
Maykon Mori <maykonmori@gmail.com>
2021-05-05 11:09:41 -0300

    att estudos

--- main.go
+++ main.go
@@ -7,13 +7,6 @@ import (
 	_ "github.com/lib/pq"
 )
 
-type Produto struct {
-	Nome       string
-	Descricao  string
-	Preco      float64
-	Quantidade int
-}
-
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -22,12 +15,5 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-
-	produtos := []Produto{
-		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
-		{"Tennis", "Confortável", 89, 3},
-		{"Fone", "Muito Bom", 59, 2},
-	}
-
 	temp.ExecuteTemplate(w, "Index", produtos)
 }
---
+++ models/produtos.go
@@ -0,0 +1,40 @@
+package models
+
+type Produto struct {
+	Id         int
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
+func buscaTodosOsProdutos() []Produto {
+	db := conectaComBaseDeDados()
+
+	selectDeTodosOsProdutos, err := db.Query("select * from produtos")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	for selectDeTodosOsProdutos.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
+	}
+	defer db.Close()
+}
